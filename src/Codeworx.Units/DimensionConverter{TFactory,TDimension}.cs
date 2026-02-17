9dce170f957ed50ea46d832b49fc100b57caf9be
Thomas Beer <thomas.beer@codemand.at>
2026-02-17 10:08:22 +0100

    Fixed implementation of DimensionTypeConverter

--- src/Codeworx.Units/DimensionConverter{TFactory,TDimension}.cs
+++ src/Codeworx.Units/DimensionConverter{TFactory,TDimension}.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Globalization;
+using System.Linq.Expressions;
 
 namespace Codeworx.Units
 {
     public class DimensionTypeConverter<TDimension> : TypeConverter
         where TDimension : IUnitBase
     {
+        private static Func<string, TDimension> _dimensionParser;
+
+        static DimensionTypeConverter()
+        {
+            var method = typeof(TDimension).GetMethod("Parse", System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static, [typeof(string)]);
+
+            if (method == null)
+            {
+                throw new InvalidOperationException("Expected static \"Parse(string)\" method!");
+            }
+
+            var param = Expression.Parameter(typeof(string), "str");
+            var body = Expression.Call(null, method, param);
+            var expression = Expression.Lambda<Func<string, TDimension>>(body, param);
+
+            _dimensionParser = expression.Compile();
+        }
+
         public override bool CanConvertFrom(ITypeDescriptorContext? context, Type sourceType)
         {
             if (sourceType == typeof(string))
@@ -31,8 +50,7 @@ namespace Codeworx.Units
         {
             if (value is string str)
             {
-                ////var parser = new TFactory();
-                ////return parser.ParseFromFullString(str);
+                return _dimensionParser(str);
             }
 
             return value;
